961081ddd04ea3a97832801e4c12d2de435dcf4e
Deepak Kumar Nayak <deepakkumarnk89@gmail.com>
2026-09-05 23:17:52 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,12 +31,12 @@ function Home() {
     
         {
           task.map((data)=>(
-            <div className="task-container flex w-full justify-between items-center">
+            <div className="task-container flex w-full justify-between items-center mb-2">
               <h1>{data.task}</h1>
               <div className="button-container flex gap-2">
                 <button className='border-2 px-6 py-2' onClick={()=> HandleEdit(data.id)}>Edit</button>
                 <button className='border-2 px-6 py-2' onClick={()=> HandleDelete(data.id)}>Delete</button>
-                <button className='border-2 px-6 py-2'>{data.isCompleted === false ? "NotComplicated" :"Complicated"}</button>
+                <button className='border-2 px-6 py-2'>{data.isCompleted === false ? "NotCompleted" :"Complicated"}</button>
                 <button className='border-2 px-6 py-2'>{data.isCompleted === false ? "Active" :"Done n Dusted"}</button>
                 
               </div>
